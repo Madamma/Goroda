b50fea73549460be136fd1c0cae3a6382b9edbaa
Adminnn <sutinayulya@mail.ru>
2025-08-01 12:48:59 +0500

    первая версия проекта.запрос координат города

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key):
-    geocoder = OpenCageGeocode(key)
-    query = city
-    results = geocoder.geocode(query)
-    if results:
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-    else:
-        return 'Город не найден'
+    try:
+        geocoder = OpenCageGeocode(key)
+        query = city
+        results = geocoder.geocode(query)
+        if results:
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+        else:
+            return 'Город не найден'
+    except Exception as e:
+        
 
 
 key = '3ebb6a43f72d4de090e915d62e03e30e'
